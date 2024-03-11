1b781ad4ff3fbe37dac8149025c2211ff84b2cc9
Giovanni Raso <126286036+GiovanniRaso@users.noreply.github.com>
2024-03-11 16:41:37 -0400

    Check link function
    using api to check a link

--- popup.js
+++ popup.js
@@ -10,8 +10,31 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 function checklink(url){
-    const apiKey = "Your API Key";
-    const safeBrowsingURL = "" ;
+    const apiKey = "857c3dfb1ef7451b3f6c0c7e1782a137831a02d18be34b123c7d3bf495c7236b";
+    const urlEncoded = "encodeURICompent(url)"; 
+    const requestUrl = 'https://www.virustotal.com/api/v3/urls';
+
+    const urlBase64 = btoa(urlEncoded).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+
+    fetch(requestUrl, {
+        method: 'POST',
+        headers: {
+            'x-apikey': apiKey,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+            "url": urlBase64
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            document.getElementById('result').innerHTML = 'URL check complete';
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            document.getElementById('result').innerHTML = 'Error';
+        })
+    })
     
 }
 
